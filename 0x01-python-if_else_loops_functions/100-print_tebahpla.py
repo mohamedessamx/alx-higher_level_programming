867e2f1958e794edea7978c0cd9021e53bc75b8f
mohamedessamx <me8624708@gmail.com>
2023-10-03 17:03:20 -0700

    l

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-def print_alphabet_reverse():
-    for i in range(ord('z'), ord('A') - 1, -1):
-        print("{:c}".format(i), end=' ')
+for i in range(ord('z'), ord('A') - 1, -1):
+    print("{:c}".format(i), end=' ')
